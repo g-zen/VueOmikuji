847263f3fb8bad9312cb9724367186ed312244e1
GuppyTubasa <39458650+GuppyTubasa@users.noreply.github.com>
2020-07-31 16:20:38 +0900

    見た目を調整

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,13 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: #ecf0f1;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
--- src/components/Omikuji.vue
+++ src/components/Omikuji.vue
@@ -1,5 +1,6 @@
 <template>
   <div class='omikuji-app'>
+    <h1 class="title">Vueおみくじ</h1>
     <h1 class='fortune'>{{ result }}</h1>
     <button class='btn'
             @click="drawFortune">おみくじを引く</button>
@@ -16,7 +17,7 @@ export default {
         '凶',
         '大凶',
       ],
-      result: 'Vueおみくじ',
+      result: '',
     };
   },
   methods: {
@@ -27,3 +28,31 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+  .omikuji-app {
+    background-color: #34495e;
+    border-radius: 10px;
+    display: inline-block;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    width: 300px;
+  }
+  .title {
+    color: #fff;
+  }
+  .fortune {
+    background-color: #fff;
+    border-radius: 8px;
+    display: block;
+    height: 50px;
+    margin: 15px;
+  }
+  .btn {
+    display: block;
+    background-color: #fff;
+    height: 40px;
+    margin: 15px;
+    width: 90%;
+  }
+</style>

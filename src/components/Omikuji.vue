66f23184d41ee558f12689cc4f93fff5222ae129
GuppyTubasa <39458650+GuppyTubasa@users.noreply.github.com>
2020-07-31 15:49:00 +0900

    おみくじアプリのコンポーネントを追加

---
+++ src/components/Omikuji.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class='omikuji-app'>
+    <h1 class='fortune'>{{ result }}</h1>
+    <button class='btn'
+            @click="drawFortune">おみくじを引く</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fortunes: [
+        '大吉',
+        '吉',
+        '凶',
+        '大凶',
+      ],
+      result: 'Vueおみくじ',
+    };
+  },
+  methods: {
+    drawFortune() {
+      const index = Math.floor(Math.random() * this.fortunes.length);
+      this.result = this.fortunes[index];
+    },
+  },
+};
+</script>
